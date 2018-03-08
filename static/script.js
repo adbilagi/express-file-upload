ba80790f3d38ed519ac82bd1317c77758e029c69
Dr Umesh R Bilagi <umeshbilagi@gmail.com>
2018-03-08 15:23:45 +0530

    async remived

--- static/script.js
+++ static/script.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
     $("form").submit(function(evt){	 
         evt.preventDefault();
         var formData = new FormData($(this)[0]);
+
+
      $.ajax({
          url: '/upload',
          type: 'POST',
          data: formData,
-         async: false,
          cache: false,
          contentType: false,
          enctype: 'multipart/form-data',
